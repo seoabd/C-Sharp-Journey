0abb0b626175c6d280d1b35a5dbf890c29212bf7
seoabd <abdulisead@icloud.com>
2024-10-05 19:24:36 +0200

    Add files via upload

---
+++ IF Statement Exercise.cs
@@ -0,0 +1,12 @@
+﻿// if Statemen Exercise
+
+var userChoice = Console.ReadLine();
+if (userChoice == "ABC")
+{
+    Console.WriteLine("The User Typed ABC");
+}
+else
+{   
+    Console.WriteLine("The User did not Type ABC");
+}
+Console.ReadKey();
