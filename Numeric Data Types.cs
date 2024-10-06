51d7f68388313bd05c8fd93d96d3efe1350eef66
seoabd <abdulisead@icloud.com>
2024-10-06 13:17:41 +0200

    Add files via upload

---
+++ Numeric Data Types.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            // or we can use this form:
+            // int age;
+            // age = 23;
+            Console.WriteLine(age);
+            //extra
+            Console.WriteLine(int.MinValue);  // We can print how small are the numbers
+            Console.WriteLine(int.MaxValue); //  We can print how big are the numbers 
+
+            long bigNumber = 900000000L;
+
+            Console.WriteLine(bigNumber);
+
+            Console.WriteLine(long.MinValue);
+            Console.WriteLine(long.MaxValue);
+            
+            Console.ReadLine(); // So the Application doesn´t close immediately
+        }
+    }
+}
