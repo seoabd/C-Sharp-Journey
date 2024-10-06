386d0d3c9db8d8b0445df5bc316c85348e6beab2
seoabd <abdulisead@icloud.com>
2024-10-06 13:36:44 +0200

    Add files via upload

---
+++ FLOAT DECIMAL DOUBLE.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double negative = -22.2D;
+
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            float precision = 4.00001F;
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+
+            decimal money = 17.99M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+
+
+            Console.ReadLine(); // So the Application doesn´t close immediately
+        }
+    }
+}
