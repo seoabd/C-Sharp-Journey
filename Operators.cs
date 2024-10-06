cb5097f44aaae050aa05812da9ae59556c6ba225
seoabd <abdulisead@icloud.com>
2024-10-06 14:47:02 +0200

    Add files via upload

---
+++ Operators.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            age++;
+            
+            Console.WriteLine(age);
+            age--;
+            Console.WriteLine(age);
+            
+
+            Console.ReadLine(); // So the Application doesn´t close immediately
+        }
+    }
+}
