5e49c4efd677ee25ed6cc8b6cb9fe27f47950213
seoabd <abdulisead@icloud.com>
2024-10-06 13:48:46 +0200

    Add files via upload

---
+++ TEXT BASED DATA TYPES.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aba";
+            char letter = 'a';
+            Console.Write("Your name is ");
+            Console.Write(name);
+
+            // or  Console.WriteLine("Your name is "+ name);
+            //we leave a space so it char value doesnt attach to the string
+
+            Console.WriteLine();
+            Console.Write(letter);
+           
+            Console.ReadLine(); // So the Application doesn´t close immediately
+        }
+    }
+}
