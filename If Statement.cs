8c744b9f7a53222f910987c4e1f02b5b80063187
seoabd <abdulisead@icloud.com>
2024-10-06 17:31:46 +0200

    Add files via upload

---
+++ If Statement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInputOutput
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+
+            //Console.WriteLine(name);
+
+            Console.WriteLine(); // if we want a gap between two questions!
+
+            Console.Write("What is your age: ");
+            string inputAge = Console.ReadLine();
+            int age = Convert.ToInt32(inputAge);
+
+            //Console.WriteLine(age);
+
+            Console.Write("Your name is " + name + " and your age is " + age);
+
+            if (age >= 18 && age <= 30)
+            {
+                Console.WriteLine();
+                Console.Write("You are between 18 and 30");
+            }
+            else if (age >= 31)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Your are definetely older than 30");
+            }
+
+            Console.ReadLine(); // So the Application doesn´t close immediately
+        }
+    }
+}
