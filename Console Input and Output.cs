4d84a71a0e05a0d1f5c09e52e554b5fdc9112d47
seoabd <abdulisead@icloud.com>
2024-10-06 17:02:47 +0200

    Add files via upload

---
+++ Console Input and Output.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInputOutput
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+            
+            //Console.WriteLine(name);
+            
+            Console.WriteLine(); // if we want a gap between two questions!
+            
+            Console.Write("What is your age: ");
+            string age = Console.ReadLine();
+            
+            //Console.WriteLine(age);
+
+            Console.Write("Your name is " + name + " and your age is " + age);
+            
+
+            Console.ReadLine(); // So the Application doesn´t close immediately
+        }
+    }
+}
