c442e56379b89a4fd49ae301d9cffba72ad9adc3
seoabd <abdulisead@icloud.com>
2024-10-06 14:59:00 +0200

    Update Operators.cs

--- Operators.cs
+++ Operators.cs
@@ -14,9 +14,25 @@ namespace Operators
             age++;  // age = age + 1;     or  age += 1;
                     // age *= 10 ;        or  age /= 10;
             Console.WriteLine(age);
+            // if you do division the best operation is as follows:
+
+            double age = 23;
+            age /= 10;   // the result gonna be precise = 2.3 and not 2 (with int).
+            Console.WriteLine(age);
+
+            
             age--;   // age = age - 1;    or age -= 1;
             Console.WriteLine(age);
-            
+
+
+            string name = "Aba";
+            name += " is programming";
+            Console.WriteLine(name);   // the result gonna be "Aba is programming"
+
+            // But it is not the same with Char!
+
+            char ch = 'A'
+            ch += 'B'  // the output is gonna be the uni code values for both of these items and try add them together as integer!
 
             Console.ReadLine(); // So the Application doesn´t close immediately
         }
