09fa34aff708efc3382d30be5f825ed944d2c2d1
seoabd <abdulisead@icloud.com>
2024-10-09 20:02:07 +0200

    Add files via upload

---
+++ For Loops.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forfLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What word do you want to say?: ");
+            string message = Console.ReadLine();
+            
+            Console.Write("How many time do you want to say the word: ");
+            int loopcounter = Convert.ToInt32(Console.ReadLine());
+
+            if (loopcounter <= 0)
+            {
+                Console.WriteLine("Please enter a number above 0");
+            }
+            else
+            {
+                for (int i = 0; i < loopcounter; i++)
+                {
+                    Console.WriteLine(message);
+                }
+            }
+            Console.ReadLine(); // So the Application doesn´t close immediately
+        }
+    }
+}
