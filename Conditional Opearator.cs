ab77baa694b314c2867eef3dd514cc6e0c4d3fa4
Sead Abduli <abdulisead@icloud.com>
2024-10-10 22:28:27 +0200

    Add files via upload

---
+++ Conditional Opearator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+
+            /*if (age >= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }*/
+
+            // conditions ? true : false
+
+            string answer = age >= 0 ? "Valid" : "Invalid";
+            Console.WriteLine(answer);
+                
+            Console.ReadLine();
+        }
+    }
+}
