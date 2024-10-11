9bdac997bb185161a51195af9a116762b2c4aac6
Sead Abduli <abdulisead@icloud.com>
2024-10-11 23:22:09 +0200

    Add files via upload

---
+++ Fizz Buzz Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzbuzzGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please enter a number: ");
+            int UserType = Convert.ToInt32(Console.ReadLine());
+
+            if ((UserType % 3 == 0) && (UserType % 5 == 0))
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (UserType % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (UserType % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(UserType);
+            }
+            Console.ReadLine();
+        }
+    }
+}
