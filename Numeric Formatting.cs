f5a676eb99668f58b00a69c6fcb1542e92a7ab10
Sead Abduli <abdulisead@icloud.com>
2024-10-11 21:32:37 +0200

    Add files via upload

---
+++ Numeric Formatting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double value = 1000D / 12.34D;
+
+            Console.WriteLine(value); // will show the whole numeric format!
+            Console.WriteLine(string.Format("{0:0.00}", value));
+                                                //after the first number it will show 2 numeric values rounded
+            Console.ReadLine();
+        }
+    }
+}
