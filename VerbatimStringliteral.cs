2fa0d43c450711baeb812ab2415734177908814e
Sead Abduli <abdulisead@icloud.com>
2024-10-12 08:46:42 +0100

    Add files via upload

---
+++ VerbatimStringliteral.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Security;
+
+namespace VerbatimStringLiteral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // \t \n \" \\ = \
+
+            string speech = "He said" \"something\"";
+            string path = "C:\\Users\\Benan\\Pictures\\Screenshots\nNew line test";
+            Console.WriteLine(speech);
+            Console.WriteLine(path);
+
+            //$ +
+            //$"Your name is {name}
+            //"Your name is " + name
+
+            path = @"C:\\Users\\Benan\\Pictures\\Screenshots\nNew line test" + "\nNew line tests";
+            Console.WriteLine(path);
+
+            string name = @"Hello ""someone""";
+            Console.WriteLine(name);
+
+            name = "Hello `someone´";
+            Console.WriteLine(name);
+
+            Console.ReadLine(); 
+        }
+    }
+}
