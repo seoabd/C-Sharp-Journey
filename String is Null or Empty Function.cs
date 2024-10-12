6ada34a244f46b94fa7d4d262da40879587f093b
Sead Abduli <abdulisead@icloud.com>
2024-10-12 17:40:01 +0100

    Add files via upload

---
+++ String is Null or Empty Function.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.ComponentModel;
+
+
+namespace StringisNullorEmptyfunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write($"Yourname is {name}");
+
+            Console.WriteLine();
+
+            if (!string.IsNullOrEmpty(name)) // null ""
+            {
+                Console.WriteLine("2");
+            }
+            Console.ReadLine();
+        }
+    }
+}
