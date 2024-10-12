198b723fceedeb45f19f175d6a5faf76ea10b04f
Sead Abduli <abdulisead@icloud.com>
2024-10-12 09:08:12 +0100

    Add files via upload

---
+++ Stringformatting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerbatimStringLiteral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aba";
+            int age = 32;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Name: " + name + "\nAge: " + age);  // "\n" produces a new line
+            Console.WriteLine("Name: {0}\nAge: {1}",name, age);
+
+            Console.WriteLine();    
+
+            Console.WriteLine("Your name is " + name + ", and your age is " + age);
+            Console.WriteLine("Your name is {0}, and your age is {1}", name, age);   // for the code to look more complete.
+
+            Console.ReadLine();
+        }
+    }
+}
