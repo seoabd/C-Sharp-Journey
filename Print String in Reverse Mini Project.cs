e77b78cae256dfbf5dbd202f50a8a40f03fb8458
Sead Abduli <abdulisead@icloud.com>
2024-10-12 19:19:54 +0100

    Add files via upload

---
+++ Print String in Reverse Mini Project.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.ComponentModel;
+
+
+namespace Exercise
+{
+    class Program
+    {
+        //Ask a user to input message
+        //Print in order
+        //Print in reverse
+        
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your full name please: ");
+            string name = Console.ReadLine();
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write(name[i]);
+                Console.WriteLine();
+                
+            }
+                Console.WriteLine();
+
+          
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.Write(name[i]);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
