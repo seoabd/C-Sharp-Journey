05e14fb408031d5c82ebd593c1aea4e1ed3350f5
Sead Abduli <abdulisead@icloud.com>
2024-10-12 17:08:43 +0100

    Add files via upload

---
+++ StringIteration Mini Project.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace StringIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "C# is awsome!";
+
+            // String Iteration (Loop)
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                Thread.Sleep(300);
+            }
+            Console.ReadLine();
+        }
+    }
+}
