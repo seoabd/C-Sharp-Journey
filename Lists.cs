e64177767c661470b917fc4b5f4265769860ec31
Sead Abduli <abdulisead@icloud.com>
2024-10-20 10:39:12 +0100

    Add files via upload

---
+++ Lists.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Text;
+using System.Numerics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Diagnostics.CodeAnalysis;
+
+
+
+namespace Lists
+
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+
+        {
+
+            int[] numbers = new int[3]
+            {
+                1, 2, 3
+            };
+
+            List<int> listNumbers = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter a number: ");
+                listNumbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < listNumbers.Count; i++)
+            {
+                Console.Write(listNumbers[i]);
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+
+}
